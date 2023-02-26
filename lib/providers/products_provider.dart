104bf8447d406ab6073a0fe240a1596b7be7d766
menna walid <mennawalidrahma@gmail.com>
2023-02-26 19:36:05 +0200

    getting products from firebase

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -6,67 +6,67 @@ import 'package:shopping/providers/products.dart';
 import 'package:http/http.dart' as http;
 
 class Products with ChangeNotifier {
-  final List<ProductInfo> _items = [
-    ProductInfo(
-        id: "F1",
-        name: "black handbag, big - leather",
-        price: 500.00,
-        imageURl:
-            "https://cdn.pixabay.com/photo/2015/08/10/20/14/handbag-883113_960_720.jpg",
-        description: "black shoulder bag, leather and very nice"),
-    ProductInfo(
-        id: "F2",
-        name: "Keyboard",
-        price: 250.50,
-        imageURl:
-            "https://cdn.pixabay.com/photo/2013/07/13/14/06/keyboard-162134_960_720.png",
-        description: "white wired keyboard - english"),
-    ProductInfo(
-        id: "F3",
-        name: "Bottle",
-        price: 150.00,
-        imageURl:
-            "https://cdn.pixabay.com/photo/2022/06/30/04/25/bottle-7292903_960_720.jpg",
-        description: "reusable bottle in 4 colors"),
-    ProductInfo(
-        id: "F4",
-        name: "Headphones",
-        price: 700.00,
-        imageURl:
-            "https://cdn.pixabay.com/photo/2013/07/13/12/17/headphone-159569_960_720.png",
-        description: "black overear headphones"),
-    ProductInfo(
-      id: 'p1',
-      name: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageURl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    ProductInfo(
-      id: 'p2',
-      name: 'Trousers, black, straight leg style',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageURl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    ProductInfo(
-      id: 'p3',
-      name: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 19.99,
-      imageURl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    ProductInfo(
-      id: 'p4',
-      name: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageURl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),
+  List<ProductInfo> _items = [
+    // ProductInfo(
+    //     id: "F1",
+    //     name: "black handbag, big - leather",
+    //     price: 500.00,
+    //     imageURl:
+    //         "https://cdn.pixabay.com/photo/2015/08/10/20/14/handbag-883113_960_720.jpg",
+    //     description: "black shoulder bag, leather and very nice"),
+    // ProductInfo(
+    //     id: "F2",
+    //     name: "Keyboard",
+    //     price: 250.50,
+    //     imageURl:
+    //         "https://cdn.pixabay.com/photo/2013/07/13/14/06/keyboard-162134_960_720.png",
+    //     description: "white wired keyboard - english"),
+    // ProductInfo(
+    //     id: "F3",
+    //     name: "Bottle",
+    //     price: 150.00,
+    //     imageURl:
+    //         "https://cdn.pixabay.com/photo/2022/06/30/04/25/bottle-7292903_960_720.jpg",
+    //     description: "reusable bottle in 4 colors"),
+    // ProductInfo(
+    //     id: "F4",
+    //     name: "Headphones",
+    //     price: 700.00,
+    //     imageURl:
+    //         "https://cdn.pixabay.com/photo/2013/07/13/12/17/headphone-159569_960_720.png",
+    //     description: "black overear headphones"),
+    // ProductInfo(
+    //   id: 'p1',
+    //   name: 'Red Shirt',
+    //   description: 'A red shirt - it is pretty red!',
+    //   price: 29.99,
+    //   imageURl:
+    //       'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    // ),
+    // ProductInfo(
+    //   id: 'p2',
+    //   name: 'Trousers, black, straight leg style',
+    //   description: 'A nice pair of trousers.',
+    //   price: 59.99,
+    //   imageURl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    // ),
+    // ProductInfo(
+    //   id: 'p3',
+    //   name: 'Yellow Scarf',
+    //   description: 'Warm and cozy - exactly what you need for the winter.',
+    //   price: 19.99,
+    //   imageURl:
+    //       'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+    // ),
+    // ProductInfo(
+    //   id: 'p4',
+    //   name: 'A Pan',
+    //   description: 'Prepare any meal you want.',
+    //   price: 49.99,
+    //   imageURl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    // ),
   ];
 
   final List<ProductInfo> _favItems = [];
@@ -94,6 +94,41 @@ class Products with ChangeNotifier {
 
   ProductInfo findById(String id) {
     return _items.firstWhere((product) => product.id == id);
+  }
+
+  Future<void> fetchAndSetProducts() async {
+    const url =
+        'https://shopping-ae175-default-rtdb.firebaseio.com/products.json';
+    try {
+      final response = await http.get(
+        Uri.parse(url),
+      );
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<ProductInfo> loadedProducts = [];
+
+      extractedData.forEach(
+        (prodId, prodData) {
+          loadedProducts.insert(
+            0,
+            ProductInfo(
+                id: prodId,
+                name: prodData['title'],
+                description: prodData['description'],
+                price: prodData['price'],
+                imageURl: prodData['imageUrl'],
+                isFavorite: prodData['isFavorite']),
+          );
+        },
+      );
+
+      _items = loadedProducts;
+      notifyListeners();
+      print(
+        json.decode(response.body),
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> addProduct(ProductInfo product) async {
--- lib/screens/products_list.dart
+++ lib/screens/products_list.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopping/widgets/app_drawer.dart';
 import 'package:shopping/widgets/main_app_bar.dart';
 import 'package:shopping/widgets/products_grid.dart';
+
+import '../providers/products_provider.dart';
 
 // ignore: must_be_immutable, use_key_in_widget_constructors
 class ProductsListScreen extends StatefulWidget {
@@ -11,13 +14,35 @@ class ProductsListScreen extends StatefulWidget {
 
 class _ProductsListScreenState extends State<ProductsListScreen> {
   final _showOnlyfav = false;
+  var _isinit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    if (_isinit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isinit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MainAppBar("Products"),
       drawer: const MyDrawer(),
-      body: ProductsGrid(_showOnlyfav),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(_showOnlyfav),
     );
   }
 }

cf1a5261616f752b7af7fdcd1807be9b41e7e0fe
menna walid <mennawalidrahma@gmail.com>
2023-03-02 03:23:39 +0200

    http for getting orders and adding one.

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shopping/providers/cart.dart';
+import 'package:http/http.dart' as http;
 
 class OrderItems {
   final String? id;
@@ -15,23 +18,88 @@ class OrderItems {
 }
 
 class Order with ChangeNotifier {
-  final List<OrderItems> _orders = [];
+  List<OrderItems> _orders = [];
 
   List<OrderItems> get orders {
     return [..._orders];
   }
 
-  void addOrder({
-    required double total,
-    required List<CartItem> cartProducts,
-  }) {
-    _orders.insert(
+  Future<void> getAndFetchOrders() async {
+    const url =
+        'https://shopping-ae175-default-rtdb.firebaseio.com/orders.json';
+    final response = await http.get(
+      Uri.parse(url),
+    );
+
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    print(extractedData);
+    final List<OrderItems> loadedOrders = [];
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, order) {
+      loadedOrders.insert(
         0,
         OrderItems(
-            amount: total,
-            dateTime: DateTime.now(),
-            id: DateTime.now().toString(),
-            products: cartProducts));
+          amount: order['amount'],
+          dateTime: DateTime.parse(order['dateTime']),
+          id: orderId,
+          products: (order['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                  cartItemId: item['id'],
+                  imageURl: item['imageURl'],
+                  name: item['title'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                ),
+              )
+              .toList(),
+        ),
+      );
+    });
+    _orders = loadedOrders;
     notifyListeners();
+  }
+
+  Future<void> addOrder({
+    required double total,
+    required List<CartItem> cartProducts,
+  }) async {
+    const url =
+        'https://shopping-ae175-default-rtdb.firebaseio.com/orders.json';
+    final timeStamp = DateTime.now();
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        body: json.encode(
+          {
+            'amount': total,
+            'dateTime': timeStamp.toIso8601String(),
+            'products': cartProducts
+                .map((cartProduct) => {
+                      'id': cartProduct.cartItemId,
+                      'title': cartProduct.name,
+                      'price': cartProduct.price,
+                      'imageURl': cartProduct.imageURl,
+                      'quantity': cartProduct.quantity,
+                    })
+                .toList(),
+          },
+        ),
+      );
+      _orders.insert(
+        0,
+        OrderItems(
+          amount: total,
+          dateTime: timeStamp,
+          id: json.decode(response.body)['name'],
+          products: cartProducts,
+        ),
+      );
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,39 +5,67 @@ import 'package:shopping/providers/orders.dart';
 import 'package:shopping/widgets/app_drawer.dart';
 import 'package:shopping/widgets/order_item.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders_screen';
 
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Order>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "My Orders",
-          style: TextStyle(
-            color: ColorPalettes.darker,
+        appBar: AppBar(
+          title: const Text(
+            "My Orders",
+            style: TextStyle(
+              color: ColorPalettes.darker,
+            ),
           ),
+          iconTheme: const IconThemeData(color: Colors.brown),
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(0, 184, 43, 43),
         ),
-        iconTheme: const IconThemeData(color: Colors.brown),
-        elevation: 0,
-        backgroundColor: const Color.fromARGB(0, 184, 43, 43),
-      ),
-      drawer: const MyDrawer(),
-      body: ListView.separated(
-          itemBuilder: ((context, i) {
-            return OrderItem(
-              order: orderData.orders[i],
-            );
-          }),
-          separatorBuilder: ((context, index) {
-            return const SizedBox(
-              height: 5,
-            );
-          }),
-          itemCount: orderData.orders.length),
-    );
+        drawer: const MyDrawer(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Order>(context, listen: false).getAndFetchOrders(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('An error has occured!'),
+              );
+            } else {
+              return Consumer<Order>(
+                builder: (BuildContext context, orderData, Widget? child) {
+                  return ListView.separated(
+                      itemBuilder: ((context, i) {
+                        return OrderItem(
+                          order: orderData.orders[i],
+                        );
+                      }),
+                      separatorBuilder: ((context, index) {
+                        return const SizedBox(
+                          height: 5,
+                        );
+                      }),
+                      itemCount: orderData.orders.length);
+                },
+              );
+            }
+          },
+        ));
   }
 }

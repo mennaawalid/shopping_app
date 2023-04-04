eb5a1432e79b6a6a0b96142921e7c4770b6a273e
menna walid <mennawalidrahma@gmail.com>
2023-04-04 03:14:57 +0200

    changed the splash screen layout

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,9 +5,31 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Loading...'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Expanded(
+              child: Icon(
+                Icons.shopping_cart,
+                size: 70,
+                color: Colors.brown,
+              ),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              child:
+                  const LinearProgressIndicator(backgroundColor: Colors.brown),
+            ),
+            const Expanded(
+              child: Text(
+                'Shopping',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
